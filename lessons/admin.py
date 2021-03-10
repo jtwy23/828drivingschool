355a8e3c9d6e44eea915652127c4a61e5f4ed22e
jtwy23 <jtwy_23@hotmail.com>
2021-03-10 17:55:19 +0000

    Added more models in Lessons

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Lessons
+from .models import Lessons, Blocks, Intensive
 
 # Register your models here.
 admin.site.register(Lessons)
+admin.site.register(Blocks)
+admin.site.register(Intensive)
--- lessons/models.py
+++ lessons/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Lessons(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Lessons'
+
     lesson_image = models.ImageField(null=True, blank=True)
     lesson_image_url = models.URLField(max_length=1024, null=True, blank=True)
     lesson_name = models.CharField(max_length=200)
@@ -10,3 +14,34 @@ class Lessons(models.Model):
 
     def __str__(self):
         return self.lesson_name
+
+
+class Blocks(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Blocks'
+
+    block_image = models.ImageField(null=True, blank=True)
+    block_image_url = models.URLField(max_length=1024, null=True, blank=True)
+    block_name = models.CharField(max_length=200)
+    block_price = models.DecimalField(max_digits=6, decimal_places=2)
+    block_description = models.TextField()
+
+    def __str__(self):
+        return self.block_name
+
+
+class Intensive(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Intensive'
+
+    intensive_image = models.ImageField(null=True, blank=True)
+    intensive_image_url = models.URLField(max_length=1024, null=True, blank=True)
+    intensive_name = models.CharField(max_length=200)
+    intensive_price = models.DecimalField(max_digits=6, decimal_places=2)
+    intensive_description = models.TextField()
+
+    def __str__(self):
+        return self.intensive_name
+
