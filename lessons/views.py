3fd0482f47c6a025daeb5f87e03a85ff016e1ff9
jtwy23 <jtwy_23@hotmail.com>
2021-03-10 18:32:08 +0000

    Added lesson views and template

--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Lessons, Blocks, Intensive
 
 # Create your views here.
+
+
+def all_lessons(request):
+    """ A view to show all lessons """
+
+    lessons = Lessons.objects.all()
+    blocks = Blocks.objects.all()
+    intensive = Intensive.objects.all()
+
+    context = {
+        'lessons': lessons,
+        'blocks': blocks,
+        'intensive': intensive,
+    }
+
+    return render(request, 'lessons/lessons.html', context)
