a60217950c8452aa15b325c459099f44c3c99f00
jtwy23 <jtwy_23@hotmail.com>
2021-03-17 15:05:30 +0000

    Created models and views for block and intensive lessons

--- lessons/urls.py
+++ lessons/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_lessons, name='lessons')
+    path('', views.all_lessons, name='lessons'),
+    path('<int:pk>', views.lesson_info, name='lesson_info'),
+    path('block/<int:pk>', views.block_info, name='block_info'),
+    path('intensive/<int:pk>', views.intensive_info, name='intensive_info'),
 ]
--- lessons/views.py
+++ lessons/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Lessons
+from django.shortcuts import render, get_object_or_404
+from .models import Lessons, Blocks, Intensive
 
 # Create your views here.
 
@@ -8,9 +8,50 @@ def all_lessons(request):
     """ A view to show all lessons """
 
     lessons = Lessons.objects.all()
+    blocks = Blocks.objects.all()
+    intensives = Intensive.objects.all()
 
     context = {
         'lessons': lessons,
+        'blocks': blocks,
+        'intensives': intensives,
     }
 
     return render(request, 'lessons/lessons.html', context)
+
+
+def lesson_info(request, pk):
+    """ A view to show lesson information """
+
+    lesson = get_object_or_404(Lessons, pk=pk)
+
+    context = {
+        'lesson': lesson,
+    }
+
+    return render(request, 'lessons/lesson_info.html', context)
+
+
+def block_info(request, pk):
+    """ A view to show block information """
+
+    block = get_object_or_404(Blocks, pk=pk)
+
+    context = {
+        'block_lesson': block,
+    }
+
+    return render(request, 'blocks/block_info.html', context)
+
+
+def intensive_info(request, pk):
+    """ A view to show intensive information """
+
+    intensive = get_object_or_404(Intensive, pk=pk)
+
+    context = {
+        'intensive': intensive,
+    }
+
+    return render(request, 'intensive/intensive_info.html', context)
+
