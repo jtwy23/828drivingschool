25e1564b73df35605fd14bd3189f78edd912053e
jtwy23 <jtwy_23@hotmail.com>
2021-03-23 23:15:11 +0000

    Added functionality to bag app

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,3 +7,20 @@ def view_bag(request):
     """ A view to render the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, lesson_id):
+    """ Add a quantity of the specified lessons to the bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if lesson_id in list(bag.keys()):
+        bag[lesson_id] += quantity
+    else:
+        bag[lesson_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
